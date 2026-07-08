c6cc20fbc47d2928ab9b9851e5530ae6373cac26
Alejandro Ramirez Garcia <gx_alejandro@hotmail.com>
2026-07-07 20:33:33 -0500

    fix: add R2 domain to admin CSP img-src directive

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,16 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://resources.bogdev.com.co'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
