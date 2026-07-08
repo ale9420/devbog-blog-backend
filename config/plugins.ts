c83f1522c7dae57012e643fa8a79ad9a50e18a22
Alejandro Ramirez Garcia <gx_alejandro@hotmail.com>
2026-07-07 19:30:59 -0500

    fix: use credentials object format for R2 provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,8 +5,10 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
     config: {
       provider: '@strapi/provider-upload-aws-s3',
       providerOptions: {
-        accessKeyId: env('R2_ACCESS_KEY_ID'),
-        secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+        credentials: {
+          accessKeyId: env('R2_ACCESS_KEY_ID'),
+          secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+        },
         endpoint: `https://${env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
         params: {
           Bucket: env('R2_BUCKET'),
