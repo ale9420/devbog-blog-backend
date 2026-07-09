2230b0cc7e7342fde818a9e12b40d78fb7d55946
Alejandro Ramirez Garcia <gx_alejandro@hotmail.com>
2026-07-08 20:52:23 -0500

    fix: remove image-optimizer plugin to fix 404 on default-mode endpoint

--- config/plugins.ts
+++ config/plugins.ts
@@ -28,16 +28,6 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   seo: {
     enabled: true,
   },
-  'image-optimizer': {
-    enabled: false,
-    config: {
-      defaultChoice: 'global',
-      defaultMode: 'webp',
-      webpQuality: 82,
-      jpegQuality: 80,
-      pngCompressionLevel: 9,
-    },
-  },
   comments: {
     enabled: true,
     config: {
